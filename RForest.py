dd833eb769ea2cbfcfe7f496a91051b5ed998584
Dulaj Atapattu <dulaj.r.atapattu@gmail.com>
2017-06-18 18:19:03 +0530

    Refactoring datat file paths to relative paths

--- RForest.py
+++ RForest.py
@@ -20,7 +20,9 @@ def preprocess_data(data_path, labels_path=None):
     features = ['reanalysis_specific_humidity_g_per_kg', 
                  'reanalysis_dew_point_temp_k', 
                  'station_avg_temp_c', 
-                 'station_min_temp_c']
+                 'station_min_temp_c',
+                 'reanalysis_min_air_temp_k',
+                 'reanalysis_max_air_temp_k']
     df = df[features]
     
     # fill missing values
@@ -37,14 +39,14 @@ def preprocess_data(data_path, labels_path=None):
     
     return sj, iq
 
-sj_train, iq_train = preprocess_data('E:/University/Semester 7/Data Mining/DM python/dengue_features_train.csv',
-                                    labels_path="E:/University/Semester 7/Data Mining/DM python/dengue_labels_train.csv")
+sj_train, iq_train = preprocess_data('data/dengue_features_train.csv',
+                                    labels_path="data/dengue_labels_train.csv")
 #covtype_df = h2o.import_file(os.path.realpath("E:/University/Semester 7/Data Mining/DM python/dengue_features_train.csv"))
-sj_train.to_csv("E:/University/Semester 7/Data Mining/DM python/sj_dengue_features_train.csv")
-iq_train.to_csv("E:/University/Semester 7/Data Mining/DM python/iq_dengue_features_train.csv")
+sj_train.to_csv("data/sj_dengue_features_train.csv")
+iq_train.to_csv("data/iq_dengue_features_train.csv")
 
-sj_train_h2o = h2o.import_file(os.path.realpath("E:/University/Semester 7/Data Mining/DM python/sj_dengue_features_train.csv"))
-iq_train_h2o = h2o.import_file(os.path.realpath("E:/University/Semester 7/Data Mining/DM python/iq_dengue_features_train.csv"))
+sj_train_h2o = h2o.import_file(os.path.realpath("data/sj_dengue_features_train.csv"))
+iq_train_h2o = h2o.import_file(os.path.realpath("data/iq_dengue_features_train.csv"))
 #split the data as described above
 sj_train, sj_valid, sj_test = sj_train_h2o.split_frame([0.6, 0.2], seed=1234)
 iq_train, iq_valid, iq_test = iq_train_h2o.split_frame([0.6, 0.2], seed=1234)
@@ -76,20 +78,20 @@ iq_rf_v2 = H2ORandomForestEstimator(
     seed=3000000)
 iq_rf_v2.train(iq_covtype_X, iq_covtype_y, training_frame=iq_train, validation_frame=iq_valid)
 
-sj_test,iq_test = preprocess_data('E:/University/Semester 7/Data Mining/DM python/dengue_features_test.csv')
+sj_test,iq_test = preprocess_data('data/dengue_features_test.csv')
 
-sj_test.to_csv("E:/University/Semester 7/Data Mining/DM python/sj_dengue_features_test.csv")
-iq_test.to_csv("E:/University/Semester 7/Data Mining/DM python/iq_dengue_features_test.csv")
+sj_test.to_csv("data/sj_dengue_features_test.csv")
+iq_test.to_csv("data/iq_dengue_features_test.csv")
 
-sj_test_h2o = h2o.import_file(os.path.realpath("E:/University/Semester 7/Data Mining/DM python/sj_dengue_features_test.csv"))
-iq_test_h2o = h2o.import_file(os.path.realpath("E:/University/Semester 7/Data Mining/DM python/iq_dengue_features_test.csv"))
+sj_test_h2o = h2o.import_file(os.path.realpath("data/sj_dengue_features_test.csv"))
+iq_test_h2o = h2o.import_file(os.path.realpath("data/iq_dengue_features_test.csv"))
 
 sj_final_rf_predictions = sj_rf_v2.predict(sj_test_h2o)
 iq_final_rf_predictions = iq_rf_v2.predict(iq_test_h2o)
 
 
-h2o.export_file(sj_final_rf_predictions, "E:/University/Semester 7/Data Mining/DM python/sj_benchmark.csv", force = False, parts = 1)
-h2o.export_file(iq_final_rf_predictions, "E:/University/Semester 7/Data Mining/DM python/iq_benchmark.csv", force = False, parts = 1)
+h2o.export_file(sj_final_rf_predictions, "data/sj_benchmark.csv", force = False, parts = 1)
+h2o.export_file(iq_final_rf_predictions, "data/iq_benchmark.csv", force = False, parts = 1)
 #validation set accuracy
 #sj_rf_v2.hit_ratio_table(valid=True)
 
